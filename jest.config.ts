cdca53254074175b9fcb7f345ad10cab97b54711
Ted Kesgar <t.kesgar@outlook.com>
2021-05-13 17:48:44 +0700

    build: fix jest test not working

--- jest.config.ts
+++ jest.config.ts
@@ -4,4 +4,9 @@ export default <Config.InitialOptions>{
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
+  moduleNameMapper: {
+    // TODO This is a hack to remove the `.js` extension from import.
+    // In the future, we might want to move away to use `ts-node` or something.
+    "^(\\..*)\\.js$": "$1",
+  },
 };
